d251ba8df7fb914637597e0ddf14a83710de7a40
Meloh <alec.mohamedbtssio@gmail.com>
2021-09-20 12:20:57 +0200

    Ajout de helmet dans l'app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require("helmet");
 const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config()
@@ -23,6 +24,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 })
+
+app.use(helmet());
 
 app.use(express.json());
 
